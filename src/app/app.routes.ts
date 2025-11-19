b1dad50b9841a61c6710bb9d3cc1323edbc2fb47
jovlasin <jovlasin@proton.me>
2025-11-19 12:51:27 -0500

    Adds files for class demo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home, title: 'Homepage'},
+    {path: 'about', component: About, title: 'About Us'},
+];
---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
+
+@Component({
+  selector: 'app-home',
+  imports: [Header, Footer],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
